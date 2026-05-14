f3dc24ea35fe6b9df9d68011418e69d0b83483bb
jpo08 <juanpabloordonezjimenez@gmail.com>
2026-05-14 15:49:08 -0500

    feat: init modified

--- audit_tracer/__init__.py
+++ audit_tracer/__init__.py
@@ -1,5 +1,10 @@
 # Package initialization
 from .session_tracker import init_tracker
+from .data_capture import activate as _activate_data_capture
 
 # Inicializar rastreo de sesión automático para scripts y Jupyter
 init_tracker()
+
+# HU-2.1: Activar interceptor automático de accesos a datos clínicos
+# Intercepta pd.read_csv, pd.read_excel, df[], df.query()
+_activate_data_capture()
